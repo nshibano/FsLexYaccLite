d0961fa517fbc8a54547b1804cad3b99e44589db
Nozomi Shibano <shibano.nozomi@gmail.com>
2018-11-18 04:55:31 +0900

    refactor

--- Tests/Expr/Run.fsx
+++ Tests/Expr/Run.fsx
@@ -4,14 +4,27 @@ open System.Diagnostics
 let cmd name arg =
     let info = ProcessStartInfo(name, arg)
     info.RedirectStandardOutput <- true
+    info.RedirectStandardError <- true
     info.UseShellExecute <- false
     info.CreateNoWindow <- true
     let proc = Process.Start(info)
     proc.OutputDataReceived.Add(fun x -> Console.WriteLine(x.Data))
+    proc.ErrorDataReceived.Add(fun x -> Console.Error.WriteLine(x.Data))
     proc.BeginOutputReadLine()
+    proc.BeginErrorReadLine()
     proc.WaitForExit()
-    proc.ExitCode
+    if proc.ExitCode <> 0 then failwithf "command %s %s failed with exit code %d." name arg proc.ExitCode
 
-cmd "dotnet" "run -p ..\..\FsLexLite -- --unicode Lexer.fsl --lexlib Microsoft.FSharp.Text.Lexing"
-cmd "dotnet" "run -p ..\..\FsYaccLite -- --module Test.Parser --lexlib Microsoft.FSharp.Text.Lexing --parslib Microsoft.FSharp.Text.Parsing -v --newprec Parser.fsy"
-cmd "dotnet" "run"
+let main() =
+    try
+        cmd "dotnet" @"build ..\..\FsLexLite"
+        cmd "dotnet" @"build ..\..\FsYaccLite"
+
+        cmd "dotnet" @"..\..\FsLexLite\bin\Debug\netcoreapp2.1\FsLexLite.dll --unicode Lexer.fsl --lexlib Microsoft.FSharp.Text.Lexing"
+        cmd "dotnet" @"..\..\FsYaccLite\bin\Debug\netcoreapp2.1\FsYaccLite.dll --module Test.Parser --lexlib Microsoft.FSharp.Text.Lexing --parslib Microsoft.FSharp.Text.Parsing -v --newprec Parser.fsy"
+
+        cmd "dotnet" @"run"
+    with
+        Failure msg -> Console.Error.WriteLine(msg)
+
+main()
