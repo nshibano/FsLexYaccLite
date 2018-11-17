a1a65458796ad1f19c647fbeb227d4c1550d1d69
Nozomi Shibano <shibano.nozomi@gmail.com>
2018-11-17 23:57:46 +0900

    refactor

--- Arg/Arg.fs
+++ Arg/Arg.fs
@@ -1,6 +1,7 @@
 // (c) Microsoft Corporation 2005-2009. 
 /// A simple command-line argument processor.
 module FsLexYaccLite.Arg
+open System
 
 type ArgType = 
     | ClearArg of bool ref
@@ -12,14 +13,11 @@ type ArgType =
     | UnitArg of (unit -> unit)
 
 type ArgInfo =
-    { 
-        Name : string
-        ArgType : ArgType
-        HelpText : string
-    }
+    { Name : string
+      ArgType : ArgType
+      HelpText : string }
   
-exception Bad of string
-exception HelpText of string
+exception InvalidCommandArgument of string option
 
 let getUsage specs u =
     let sbuf = new System.Text.StringBuilder 100  
@@ -37,83 +35,70 @@ let getUsage specs u =
     pstring "\t"; pstring "-help"; pstring ": "; pendline "display this list of options";
     sbuf.ToString()
 
-type ArgParser() = 
-
-    /// Parse some of the arguments given by 'argv', starting at the given position
-    [<System.Obsolete("This method should not be used directly as it will be removed in a future revision of this library")>]
-    static member ParsePartial(cursor,argv,argSpecs:seq<ArgInfo>,?other,?usageText) =
-        let other = defaultArg other (fun _ -> ())
-        let usageText = defaultArg usageText ""
-        let nargs = Array.length argv 
-        incr cursor;
-        let argSpecs = argSpecs |> Seq.toList
-        let specs = argSpecs |> List.map (fun (arg:ArgInfo) -> arg.Name, arg.ArgType)
-        while !cursor < nargs do
-          let arg = argv.[!cursor] 
-          let rec findMatchingArg args = 
-            match args with
-            | ((s, action) :: _) when s = arg -> 
-               let getSecondArg () = 
-                   if !cursor + 1 >= nargs then 
-                     raise(Bad("option "+s+" needs an argument.\n"+getUsage argSpecs usageText));
-                   argv.[!cursor+1] 
-                 
-               match action with 
-               | UnitArg f -> 
-                 f (); 
-                 incr cursor
-               | SetArg f ->
-                 f := true; 
-                 incr cursor
-               | ClearArg f -> 
-                 f := false; 
-                 incr cursor
-               | StringArg f-> 
-                 let arg2 = getSecondArg() 
-                 f arg2; 
-                 cursor := !cursor + 2
-               | IntArg f -> 
-                 let arg2 = getSecondArg () 
-                 let arg2 = try int32 arg2 with _ -> raise(Bad(getUsage argSpecs usageText)) in  
-                 f arg2;
-                 cursor := !cursor + 2;
-               | FloatArg f -> 
-                 let arg2 = getSecondArg() 
-                 let arg2 = try float arg2 with _ -> raise(Bad(getUsage argSpecs usageText)) in 
-                 f arg2; 
-                 cursor := !cursor + 2;
-               | RestArg f -> 
-                 incr cursor;
-                 while !cursor < nargs do
-                     f (argv.[!cursor]);
-                     incr cursor;
-
-            | (_ :: more)  -> findMatchingArg more 
-            | [] -> 
-                if arg = "-help" || arg = "--help" || arg = "/help" || arg = "/help" || arg = "/?" then
-                    raise (HelpText (getUsage argSpecs usageText))
-                // Note: for '/abc/def' does not count as an argument
-                // Note: '/abc' does
-                elif arg.Length>0 && (arg.[0] = '-' || (arg.[0] = '/' && not (arg.Length > 1 && arg.[1..].Contains ("/")))) then
-                    raise (Bad ("unrecognized argument: "+ arg + "\n" + getUsage argSpecs usageText))
-                else 
-                   other arg;
-                   incr cursor
-          findMatchingArg specs 
-
-    /// Parse the arguments given by System.Environment.GetEnvironmentVariables()
-    /// according to the argument processing specifications "specs".
-    /// Args begin with "-". Non-arguments are passed to "f" in
-    /// order.  "use" is printed as part of the usage line if an error occurs.
-    static member Parse (specs,?other,?usageText) = 
+let parseCommandLineArgs (specs : ArgInfo list) (other : string -> unit) (usageText : string) = 
         let current = ref 0
         let argv = System.Environment.GetCommandLineArgs() 
-        try ArgParser.ParsePartial (current, argv, specs, ?other=other, ?usageText=usageText)
-        with 
-          | Bad h 
-          | HelpText h -> 
-              System.Console.Error.WriteLine h; 
-              System.Console.Error.Flush();  
-              System.Environment.Exit(1); 
-          | e -> 
-              reraise()
+        try
+            let nargs = Array.length argv 
+            incr current;
+            let argSpecs = specs |> Seq.toList
+            let specs = argSpecs |> List.map (fun (arg:ArgInfo) -> arg.Name, arg.ArgType)
+            while !current < nargs do
+              let arg = argv.[!current] 
+              let rec findMatchingArg args = 
+                match args with
+                | ((s, action) :: _) when s = arg -> 
+                   let getSecondArg () = 
+                       if !current + 1 >= nargs then 
+                         raise(InvalidCommandArgument(Some ("option "+s+" needs an argument.")));
+                       argv.[!current+1] 
+                 
+                   match action with 
+                   | UnitArg f -> 
+                     f (); 
+                     incr current
+                   | SetArg f ->
+                     f := true; 
+                     incr current
+                   | ClearArg f -> 
+                     f := false; 
+                     incr current
+                   | StringArg f-> 
+                     let arg2 = getSecondArg() 
+                     f arg2; 
+                     current := !current + 2
+                   | IntArg f -> 
+                     let arg2 = getSecondArg () 
+                     let arg2 = try int32 arg2 with _ -> raise(InvalidCommandArgument None) in  
+                     f arg2;
+                     current := !current + 2;
+                   | FloatArg f -> 
+                     let arg2 = getSecondArg() 
+                     let arg2 = try float arg2 with _ -> raise(InvalidCommandArgument None) in 
+                     f arg2; 
+                     current := !current + 2;
+                   | RestArg f -> 
+                     incr current;
+                     while !current < nargs do
+                         f (argv.[!current]);
+                         incr current;
+
+                | (_ :: more)  -> findMatchingArg more 
+                | [] -> 
+                    if arg = "-help" || arg = "--help" || arg = "/help" || arg = "/help" || arg = "/?" then
+                        raise (InvalidCommandArgument None)
+                    // Note: for '/abc/def' does not count as an argument
+                    // Note: '/abc' does
+                    elif arg.Length>0 && (arg.[0] = '-' || (arg.[0] = '/' && not (arg.Length > 1 && arg.[1..].Contains ("/")))) then
+                        raise (InvalidCommandArgument (Some ("unrecognized argument: "+ arg)))
+                    else 
+                       other arg;
+                       incr current
+              findMatchingArg specs 
+        with InvalidCommandArgument msg -> 
+              Console.Error.WriteLine(msg)
+              match msg with
+              | Some msg -> Console.Error.WriteLine(getUsage specs usageText)
+              | None -> ()
+              Console.Error.Flush()
+              Environment.Exit(1)
