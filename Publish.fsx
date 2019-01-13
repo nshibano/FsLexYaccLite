910701f098167d3f70eded5e4d543207063c48ff
Nozomi Shibano <shibano.nozomi@gmail.com>
2019-01-13 22:51:49 +0900

    refactor

--- Publish.fsx
+++ Publish.fsx
@@ -12,22 +12,19 @@ let buildVersion =
     let s = Environment.GetEnvironmentVariable("APPVEYOR_BUILD_VERSION")
     if isNull s then "0.0.0" else s
 
-let zipFileName = sprintf "FsLexYaccLite-%s.zip" buildVersion
-
 try
     if Directory.Exists("Publish") then
         Directory.Delete("Publish", true)
 
-    cmd "dotnet" @"publish -c Release FsLexLite\FsLexLite.fsproj -o ..\Publish\FsLexLite"
-    cmd "dotnet" @"publish -c Release FsYaccLite\FsYaccLite.fsproj -o ..\Publish\FsYaccLite"
-
-    cp @"Runtime\Lexing.fs"   @"Publish\Runtime"
-    cp @"Runtime\Parsing.fs"  @"Publish\Runtime"
-
+    cmd "dotnet" @"publish -c Release FsLexLite\FsLexLite.fsproj -o ..\Publish\core21\FsLexLite"
+    cmd "dotnet" @"publish -c Release FsYaccLite\FsYaccLite.fsproj -o ..\Publish\core21\FsYaccLite"
+    cp @"Runtime\Lexing.fs"   @"Publish\core21\Runtime"
+    cp @"Runtime\Parsing.fs"  @"Publish\core21\Runtime"
+    
+    let zipFileName = sprintf "FsLexYaccLite-core21-%s.zip" buildVersion
     if File.Exists(zipFileName) then
         File.Delete(zipFileName)
-    ZipFile.CreateFromDirectory("Publish", zipFileName)
-
+    ZipFile.CreateFromDirectory("Publish\core21", zipFileName)
     try cmd "appveyor" ("PushArtifact " + zipFileName)
     with _ -> printfn "failed to run appveyor.exe"
 with
