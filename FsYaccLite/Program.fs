17f8c6b397fdf2357a929288dcd757dbae176054
Nozomi Shibano <shibano.nozomi@gmail.com>
2019-01-19 21:24:22 +0900

    refactor

--- FsYaccLite/Program.fs
+++ FsYaccLite/Program.fs
@@ -16,82 +16,80 @@ let mutable lexlib = "FsLexYaccLite.Lexing"
 let mutable parslib = "FsLexYaccLite.Parsing"
 
 let usage =
-  [ ("-o", StringArg (fun s -> output <- Some s), "Name the output file.");
-    ("-v", UnitArg (fun () -> verbose <- true), "Produce a listing file."); 
-    ("--module", StringArg (fun s -> modname <- Some s), "Define the F# module name to host the generated parser."); 
-    ("--lexlib", StringArg (fun s ->  lexlib <- s), "Specify the namespace for the implementation of the lexer (default: Microsoft.FSharp.Text.Lexing)");
-    ("--parslib", StringArg (fun s ->  parslib <- s), "Specify the namespace for the implementation of the parser table interpreter (default: Microsoft.FSharp.Text.Parsing)")]
+    [("-o", StringArg (fun s -> output <- Some s), "Name the output file.")
+     ("-v", UnitArg (fun () -> verbose <- true), "Produce a listing file.")
+     ("--module", StringArg (fun s -> modname <- Some s), "Define the F# module name to host the generated parser.")
+     ("--lexlib", StringArg (fun s ->  lexlib <- s), "Specify the namespace for the implementation of the lexer (default: Microsoft.FSharp.Text.Lexing)")
+     ("--parslib", StringArg (fun s ->  parslib <- s), "Specify the namespace for the implementation of the parser table interpreter (default: Microsoft.FSharp.Text.Parsing)")]
 
 let main() =
-  let _ = parseCommandLineArgs usage (fun x -> match input with Some _ -> failwith "more than one input given" | None -> input <- Some x) "fsyacc <filename>"
+    parseCommandLineArgs usage (fun x -> match input with Some _ -> failwith "more than one input given" | None -> input <- Some x) "fsyacc <filename>"
 
-  let input =
-    match input with
-    | Some x -> x
-    | None -> failwith "no input given"
+    let input =
+        match input with
+        | Some x -> x
+        | None -> failwith "no input given"
 
-  let spec =
-      let lexbuf =
+    let spec =
+        let lexbuf =
             use f = new FileStream(input, FileMode.Open, FileAccess.Read, FileShare.ReadWrite)
             use r = new StreamReader(f, true)
             LexBuffer.FromString(r.ReadToEnd())
       
-      lexbuf.EndPos <- Position.FirstLine(input)
+        lexbuf.EndPos <- Position.FirstLine(input)
 
-      try 
+        try 
         Parser.spec Lexer.token lexbuf 
-      with e -> 
-         eprintf "%s(%d,%d): error: %s" input lexbuf.StartPos.Line lexbuf.StartPos.Column e.Message;
-         exit 1  in
+        with e -> 
+            eprintf "%s(%d,%d): error: %s" input lexbuf.StartPos.Line lexbuf.StartPos.Column e.Message;
+            exit 1  in
   
-  let spec = { spec with Tokens = List.rev spec.Tokens }
+    let spec = { spec with Tokens = List.rev spec.Tokens }
   
-  let output =
-    match output with
-    | Some x -> x
-    | None -> Path.ChangeExtension(input, ".fs")
+    let output =
+        match output with
+        | Some x -> x
+        | None -> Path.ChangeExtension(input, ".fs")
 
-  printfn "building tables"; 
-  let preprocessed = processParserSpecAst spec
-  let compiled = compile preprocessed
-  let (code, pos) = spec.Header
+    printfn "building tables"; 
+    let preprocessed = processParserSpecAst spec
+    let compiled = compile preprocessed
+    let (code, _) = spec.Header
 
-  printfn "%d states" compiled.States.Length; 
-  printfn "%d nonterminals" compiled.GotoTable.[0].Length; 
-  printfn "%d terminals" preprocessed.Terminals.Length
-  printfn "%d productions" compiled.Productions.Length; 
-  printfn "#rows in action table: %d" compiled.ActionTable.Length
+    printfn "%d states" compiled.States.Length; 
+    printfn "%d nonterminals" compiled.GotoTable.[0].Length; 
+    printfn "%d terminals" preprocessed.Terminals.Length
+    printfn "%d productions" compiled.Productions.Length; 
+    printfn "#rows in action table: %d" compiled.ActionTable.Length
+    YaccOutput.outputParser input output modname lexlib parslib code spec preprocessed compiled
 
-  if verbose then
-      use f = (File.CreateText (input + ".html")) :> TextWriter
-      fprintfn f "<pre>"
-      fprintfn f "Output file describing compiled parser placed in %s" output
-      YaccOutput.outputCompilationReport f preprocessed compiled
-      fprintfn f "</pre>"
+    if verbose then
+        use f = (File.CreateText (input + ".html")) :> TextWriter
+        fprintfn f "<pre>"
+        fprintfn f "Output file describing compiled parser placed in %s" output
+        YaccOutput.outputCompilationReport f preprocessed compiled
+        fprintfn f "</pre>"
 
-      YaccOutput.outputTableImages input preprocessed compiled
+        YaccOutput.outputTableImages input preprocessed compiled
 
-      let actionMatrix =
-          Array.map (fun (row : ActionTableRow) ->
+        let actionMatrix =
+            Array.map (fun (row : ActionTableRow) ->
                 let ary = Array.create preprocessed.Terminals.Length None
                 Array.iter (fun (i, _) ->
                     ary.[i] <- Some 0) row.LookaheadActions
                 ary) compiled.ActionTable
       
-      let actionHashtable = Hashtable.create None actionMatrix
-      Hashtable.outputHashtableStat actionMatrix actionHashtable
-      Hashtable.outputHashtableImage (input + "-actionHashtableImage.png") actionHashtable
+        let actionHashtable = Hashtable.create None actionMatrix
+        Hashtable.outputHashtableStat actionMatrix actionHashtable
+        Hashtable.outputHashtableImage (input + "-actionHashtableImage.png") actionHashtable
 
-      let gotoHashtable = Hashtable.create None compiled.GotoTable
-      Hashtable.outputHashtableStat compiled.GotoTable gotoHashtable
-      Hashtable.outputHashtableImage (input + "-gotoHashtableImage.png") gotoHashtable
+        let gotoHashtable = Hashtable.create None compiled.GotoTable
+        Hashtable.outputHashtableStat compiled.GotoTable gotoHashtable
+        Hashtable.outputHashtableImage (input + "-gotoHashtableImage.png") gotoHashtable
 
-  YaccOutput.outputParser input output modname lexlib parslib code spec preprocessed compiled
-
-let _ = 
-    //try
-        main()
-    //with e -> 
-    //  eprintf "FSYACC: error FSY000: %s" (match e with Failure s -> s | e -> e.Message);
-    //  exit 1
+try
+    main()
+with e -> 
+    Console.Error.WriteLine(e.Message)
+    exit 1
 
