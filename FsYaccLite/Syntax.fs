6d79bd8f6b8ece5c6a3f1e8bc3f8e91c5a674c37
Nozomi Shibano <shibano.nozomi@gmail.com>
2019-01-24 21:44:38 +0900

    refactor

--- FsYaccLite/Syntax.fs
+++ FsYaccLite/Syntax.fs
@@ -3,7 +3,16 @@ module Syntax
 type Identifier = string
 type Code = string * FsLexYaccLite.Lexing.Position
 type Rule = Rule of Identifier list * Identifier option * Code option
+
 type Associativity = LeftAssoc | RightAssoc | NonAssoc
+
+type Decl =
+    | Token of string list
+    | Type of string list
+    | Start of string list
+    | Left of string list
+    | Right of string list
+    | Nonassoc of string list
 
 type ParserSpec = 
     { Header : Code
